010d68447319f8e4c525f0396c7442a9177d9242
SGRAVANI <shakuntla.ravani@gmail.com>
2024-03-27 23:49:01 +0530

    spellchecker

---
+++ src/Components/SpellCheck.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import "./style.css"
+const customDictionary = {
+
+    teh: "the",
+  
+    wrok: "work",
+  
+    fot: "for",
+  
+    exampl: "example"
+  
+  };
+  
+export default function SpellCheck() {
+ let [ip,setIp]=useState()
+ let [suggest,setSuggestion]=useState()
+ function handleChange(e)
+ {
+    setIp(e.target.value)
+ }
+ function findSuggestion()
+ {
+    if(customDictionary.hasOwnProperty(ip))
+    {
+        setSuggestion(customDictionary[ip])
+    }
+    else{
+        setSuggestion("")
+    }
+ }
+ useEffect(()=>{
+findSuggestion()
+ },[ip])
+    return (
+    <div className='container'>
+   <h1>Spell Check and Auto-Correction</h1>
+   <textarea rows={5} cols={40} placeholder='Enter text...' value={ip} onChange={handleChange}></textarea>
+   {suggest &&<p>Did you mean: <b>{suggest}</b>?</p>}
+    </div>
+  )
+}
